1a6a6f914a41bac8cf25ab12736c52d603e9e58e
Mikem <Mikemangan00@gmail.com>
2017-07-10 14:42:45 -0400

    setup files needed.

---
+++ app/src/main/java/com/example/android/inventoryapp_udacity_project/InventoryCursorActivity.java
@@ -0,0 +1,8 @@
+package com.example.android.inventoryapp_udacity_project;
+
+/**
+ * Created by mikem on 7/10/2017.
+ */
+
+public class InventoryCursorActivity {
+}
---
+++ app/src/main/java/com/example/android/inventoryapp_udacity_project/data/InventoryProvider.java
@@ -0,0 +1,8 @@
+package com.example.android.inventoryapp_udacity_project.data;
+
+/**
+ * Created by mikem on 7/10/2017.
+ */
+
+public class InventoryProvider {
+}
